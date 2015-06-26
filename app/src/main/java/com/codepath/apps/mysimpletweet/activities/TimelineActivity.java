e3ac9eca90a0598690a4bec79909597bcf436883
bhusarisuccess <bhusari.success@gmail.com>
2015-06-26 18:15:57 +0530

    text shows the url link and refresh layout.

--- app/src/main/java/com/codepath/apps/mysimpletweet/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweet/activities/TimelineActivity.java
@@ -1,6 +1,9 @@
 package com.codepath.apps.mysimpletweet.activities;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -23,34 +26,59 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class TimelineActivity extends ActionBarActivity {
+    private SwipeRefreshLayout swipeContainer;
 
     private TwitterClient client;
     private ArrayList<Tweet> tweet;
     private ArrayAdapter atweet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
-        ActionBar bar= getSupportActionBar();
+        ActionBar bar = getSupportActionBar();
         bar.setDisplayShowHomeEnabled(true);
-        bar.setIcon(R.drawable.twitter_icon);
-       // bar.setBackgroundDrawable(Android.Graphics.Color.ParseColor("#FF6A00"));
+        bar.setIcon(R.drawable.twitter_logo);
+      // bar.setBackgroundDrawable(new BitmapDrawable(BitmapFactory.decodeResource(getResources(),R.color.abc_background_cache_hint_selector_material_dark)));
+        bar.setBackgroundDrawable(new ColorDrawable(Color.BLUE));
         //Find the listview
-        ListView ivTweet= (ListView) findViewById(R.id.lvTweets);
+        ListView ivTweet = (ListView) findViewById(R.id.lvTweets);
         //create arraylist
-        tweet= new ArrayList<>();
+        tweet = new ArrayList<>();
         //construct the Adapter
-        atweet= new TweetArrayAdapter(this,tweet);
+        atweet = new TweetArrayAdapter(this, tweet);
         //connect adapter to listview
         ivTweet.setAdapter(atweet);
         //get the client
-        client= TwitterApplication.getRestClient();  // singletone client
-        poulateTimeLine();
+        client = TwitterApplication.getRestClient();  // singletone client
+        populateTimeLine();
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                populateTimeLine();
+            }
+        });
+       // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
     }
+
+
+
+
     //send the api request to get json responce
     //
-    private void poulateTimeLine() {
+    private void populateTimeLine() {
         client.getHomeTimeLine(new JsonHttpResponseHandler(){
             //success
             @Override
@@ -62,6 +90,8 @@ public class TimelineActivity extends ActionBarActivity {
                 //Create a models
                 //load data into listview
                atweet.addAll(Tweet.fromJSONArray(json));
+                swipeContainer.setRefreshing(false);
+
             }
             //failuar
 
@@ -87,7 +117,7 @@ public class TimelineActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_tweet) {
             return true;
         }
 
